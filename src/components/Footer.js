c1a531e722133a7411cc7fa447bde59fef088b0c
Ayush451 <88429944+Ayush451@users.noreply.github.com>
2022-08-06 13:22:39 +0100

    Completed This project

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+export default function Footer(){
+    return(
+        <div className="footer container">
+            <div className="footer-section">
+                <p className="title">FoodiesHub.com</p>
+                <p> FoodiesHub is a place where you can please your soul and tummy with delicious food and recepies of all cuisine. And out service is absolutely free.</p>
+                <p>&copy; 2022 | All Rights Reserved</p>
+            </div>
+
+            <div className="footer-section">
+                <p className="title">Contact Us</p>
+                <p>foodiehub@gmail.com</p>
+                <p>3244 239809 90980</p>
+                <p>82 Uxbridge Street</p>
+            </div>
+
+            <div className="footer-section">
+                <p className="title">Socials</p>
+                <p> Facebook</p>
+                <p> Twitter</p>
+                <p> Instagram</p>
+            </div>
+        </div>
+    )
+}
